5a54303ab8b7b7192f932a937e3824e9ae55f7fa
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-10 16:31:35 +0100

    add bonus 2 (w/brainrot for funsies)

---
+++ bonus/clock function/bonus_orario.js
@@ -0,0 +1,11 @@
+/* quanto manca:  Stabilire l'ora attuale e attraverso una operazione matematica stabilire i minuti mancanti allo scoccare della prossima ora.  Oltre a Math ci sono anche altre funzionalitá di javascript (built-in) che possaimo usare senza installazioni come ad esempio le funzioni dell'oggetto Date. */
+
+const displayTime = new Date()
+
+const timeNow = ('0' + displayTime.getHours()).slice(-2) + ":" + ('0' + displayTime.getMinutes()).slice(-2) + ":" + ('0' + displayTime.getSeconds()).slice(-2) + "." + ('00' + displayTime.getMilliseconds()).slice(-3);
+console.log(timeNow);
+
+const time2NextHour = (60 - displayTime.getMinutes()) + ":" + (60 - displayTime.getSeconds()) + "." + (1000 - displayTime.getMilliseconds())
+console.log(time2NextHour);
+
+alert("Alla prossima ora manca: " + time2NextHour)
