d3bfc5c63ca081d164f7c11ffe8ac3e6d018a3d4
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-10 17:15:13 +0100

    fix brainrot

--- bonus/clock function/bonus_orario.js
+++ bonus/clock function/bonus_orario.js
@@ -1,11 +1,14 @@
 /* quanto manca:  Stabilire l'ora attuale e attraverso una operazione matematica stabilire i minuti mancanti allo scoccare della prossima ora.  Oltre a Math ci sono anche altre funzionalitá di javascript (built-in) che possaimo usare senza installazioni come ad esempio le funzioni dell'oggetto Date. */
 
-const displayTime = new Date()
+function stimulationNeeded() {
 
-const timeNow = ('0' + displayTime.getHours()).slice(-2) + ":" + ('0' + displayTime.getMinutes()).slice(-2) + ":" + ('0' + displayTime.getSeconds()).slice(-2) + "." + ('00' + displayTime.getMilliseconds()).slice(-3);
-console.log(timeNow);
+    const displayTime = new Date()
 
-const time2NextHour = (60 - displayTime.getMinutes()) + ":" + (60 - displayTime.getSeconds()) + "." + (1000 - displayTime.getMilliseconds())
-console.log(time2NextHour);
+    const timeNow = ('0' + displayTime.getHours()).slice(-2) + ":" + ('0' + displayTime.getMinutes()).slice(-2) + ":" + ('0' + displayTime.getSeconds()).slice(-2) + "." + ('00' + displayTime.getMilliseconds()).slice(-3);
+    console.log(timeNow);
 
-alert("Alla prossima ora manca: " + time2NextHour)
+    const time2NextHour = (60 - displayTime.getMinutes()) + ":" + (60 - displayTime.getSeconds()) + "." + (1000 - displayTime.getMilliseconds())
+    console.log(time2NextHour);
+
+    alert("Alla prossima ora manca: " + time2NextHour)
+} setInterval(stimulationNeeded, 5000)
